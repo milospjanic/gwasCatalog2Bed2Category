978122c5edb82025e4a37108107ae332fb2f8c2b
Milos Pjanic <pjanic.milos@gmail.com>
2019-04-04 16:14:04 -0700

    Update gwasCatalog2Bed2Category.sh

--- gwasCatalog2Bed2Category.sh
+++ gwasCatalog2Bed2Category.sh
@@ -2,12 +2,15 @@
 #download gwas catalog and create bed file with "chr position position+1 proxy_gene phenotype"
 echo "
 #download gwas catalog 
-wget http://www.genome.gov/admin/gwascatalog.txt
-awk -F\"\t\" '{if (\$12!=\"\") print \$12\"\t\"\$13\"\t\"\$15\"\t\"\$8}' gwascatalog.txt > tmp
-awk -F\"\t\" '{print \$1\"\t\"\$2\"\t\"\$2+1\"\t\"\$3\"\t\"\$4}' tmp > GwasCatalog.bed
-rm tmp" > GwasCatalog2Bed.sh
-chmod 775 GwasCatalog2Bed.sh
-./GwasCatalog2Bed.sh
+
+wget https://www.dropbox.com/s/305f4dsbpkdtw3c/full.mod.crossmap.plus.snpsnpinteract.header
+mv full.mod.crossmap.plus.snpsnpinteract.header GwasCatalog.bed
+
+#awk -F\"\t\" '{if (\$12!=\"\") print \$12\"\t\"\$13\"\t\"\$15\"\t\"\$8}' gwascatalog.txt > tmp
+#awk -F\"\t\" '{print \$1\"\t\"\$2\"\t\"\$2+1\"\t\"\$3\"\t\"\$4}' tmp > GwasCatalog.bed
+#rm tmp" > GwasCatalog2Bed.sh
+#chmod 775 GwasCatalog2Bed.sh
+#./GwasCatalog2Bed.sh
 
 #writing main to perform grep on gwascatalog.txt.cut
 echo "
